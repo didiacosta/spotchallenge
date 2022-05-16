cdbe826edd1098541da0c6fdad2bcdd96d83d9c3
didiacosta <didi_acosta@hotmail.com>
2022-05-16 17:45:13 -0500

    model and migrations

---
+++ song/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Song(models.Model):
+
+	artistName = models.CharField(max_length = 50)
+	id = models.CharField(max_length = 12, primary_key = True)
+	name = models.CharField(max_length = 50)
+	releaseDate = models.DateField()
+	kind = models.CharField(max_length = 30)
+	artistId = models.CharField(max_length = 12)
+	artistUrl = models.CharField(max_length = 100)
+	contentAdvisoryRating = models.CharField(max_length = 30)
+	artworkUrl100 = models.CharField(max_length = 100)
+	genres = models.CharField(max_length = 200)
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		db_table = 'song'
+
+
